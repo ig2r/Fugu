cfffe96b2cc8ce1d1dc22f106564142ea6b85bb8
Stephan Deininghaus <stephan@deininghaus.com>
2017-03-29 07:54:22 +0200

    Added tests/Fugu.Core.Benchmarks project

---
+++ tests/Fugu.Core.Benchmarks/Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Fugu.Core.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+        }
+    }
+}
