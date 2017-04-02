82d7184370f6206a85e3a92d3106f497dd87c540
Stephan Deininghaus <stephan@deininghaus.com>
2017-04-02 16:53:23 +0200

    Added SelectBuilder benchmark

--- tests/Fugu.Core.Benchmarks/Program.cs
+++ tests/Fugu.Core.Benchmarks/Program.cs
@@ -1,13 +1,5 @@
-﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
-using Fugu.TableSets;
+﻿using BenchmarkDotNet.Running;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Fugu.Core.Benchmarks
 {
@@ -19,44 +11,9 @@ namespace Fugu.Core.Benchmarks
                 .AddCommandLine(args)
                 .Build();
 
-            BenchmarkRunner.Run<SimpleCommitBenchmark>();
+            //BenchmarkRunner.Run<SimpleCommitBenchmark>();
+            BenchmarkRunner.Run<SelectBuilderBenchmark>();
             //new SimpleCommitBenchmark { DegreeOfConcurrency = 1 }.Commit1000Batches().Wait();
-        }
-    }
-
-    //[MemoryDiagnoser]
-    public class SimpleCommitBenchmark
-    {
-        public int Iterations { get; set; } = 1000;
-
-        [Params(1, 2, 16)]
-        public int DegreeOfConcurrency { get; set; }
-
-        [Benchmark]
-        public async Task Commit1000Batches()
-        {
-            var iterationsPerSlice = Iterations / DegreeOfConcurrency;
-
-            var tableSet = new InMemoryTableSet();
-            using (var store = await KeyValueStore.CreateAsync(tableSet))
-            {
-                var tasks = from i in Enumerable.Range(0, DegreeOfConcurrency)
-                            let iteration = i
-                            select Task.Run(async () =>
-                            {
-                                var data = new byte[64];
-                                var offset = iteration * iterationsPerSlice;
-
-                                for (int i = 0; i < iterationsPerSlice; i++)
-                                {
-                                    var writeBatch = new WriteBatch();
-                                    writeBatch.Put(Encoding.UTF8.GetBytes($"key:{offset + i}"), data);
-                                    await store.CommitAsync(writeBatch);
-                                }
-                            });
-
-                await Task.WhenAll(tasks.ToArray());
-            }
         }
     }
 }
---
+++ tests/Fugu.Core.Benchmarks/SimpleCommitBenchmark.cs
@@ -0,0 +1,45 @@
+﻿using BenchmarkDotNet.Attributes;
+using Fugu.TableSets;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fugu.Core.Benchmarks
+{
+    //[MemoryDiagnoser]
+    public class SimpleCommitBenchmark
+    {
+        public int Iterations { get; } = 1000;
+
+        [Params(1, 2, 16)]
+        public int DegreeOfConcurrency { get; set; }
+
+        [Benchmark(OperationsPerInvoke = 1000)]
+        public async Task Commit1000Batches()
+        {
+            var iterationsPerSlice = Iterations / DegreeOfConcurrency;
+
+            var tableSet = new InMemoryTableSet();
+            using (var store = await KeyValueStore.CreateAsync(tableSet))
+            {
+                var tasks = from i in Enumerable.Range(0, DegreeOfConcurrency)
+                            let iteration = i
+                            select Task.Run(async () =>
+                            {
+                                var data = new byte[256];
+                                var offset = iteration * iterationsPerSlice;
+
+                                for (int i = 0; i < iterationsPerSlice; i++)
+                                {
+                                    var writeBatch = new WriteBatch();
+                                    writeBatch.Put(Encoding.UTF8.GetBytes($"key:{offset + i}"), data);
+                                    await store.CommitAsync(writeBatch);
+                                }
+                            });
+
+                await Task.WhenAll(tasks.ToArray());
+            }
+        }
+    }
+}
