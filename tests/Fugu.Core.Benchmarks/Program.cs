410e934e468179c9996a6b2187f168028a61645b
Stephan Deininghaus <stephan@deininghaus.com>
2017-05-25 09:14:48 +0200

    Added Fugu.Core.Benchmarks project

---
+++ tests/Fugu.Core.Benchmarks/Program.cs
@@ -0,0 +1,18 @@
+﻿using BenchmarkDotNet.Running;
+using Microsoft.Extensions.Configuration;
+
+namespace Fugu.Core.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            BenchmarkRunner.Run<SimpleCommitBenchmark>();
+            //new SimpleCommitBenchmark { DegreeOfConcurrency = 1 }.Commit1000Batches().Wait();
+        }
+    }
+}
---
+++ tests/Fugu.Core.Benchmarks/SimpleCommitBenchmark.cs
@@ -0,0 +1,49 @@
+﻿using BenchmarkDotNet.Attributes;
+using Fugu.TableSets;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fugu.Core.Benchmarks
+{
+    //[MemoryDiagnoser]
+    public class SimpleCommitBenchmark
+    {
+        public const int ITERATIONS = 1000;
+
+        [Params(1, 2, 16)]
+        public int DegreeOfConcurrency { get; set; }
+
+        [Benchmark(OperationsPerInvoke = ITERATIONS)]
+        public async Task CommitBatches()
+        {
+            var iterationsPerSlice = ITERATIONS / DegreeOfConcurrency;
+
+            var tableSet = new InMemoryTableSet();
+            using (var store = await KeyValueStore.CreateAsync(tableSet))
+            {
+                var tasks = from i in Enumerable.Range(0, DegreeOfConcurrency)
+                            let iteration = i
+                            select Task.Run(async () =>
+                            {
+                                var data = new byte[256];
+                                var offset = iteration * iterationsPerSlice;
+
+                                for (int i = 0; i < iterationsPerSlice; i++)
+                                {
+                                    var writeBatch = new WriteBatch();
+                                    writeBatch.Put(GetKey(offset + i), data);
+                                    await store.CommitAsync(writeBatch);
+                                }
+                            });
+
+                await Task.WhenAll(tasks.ToArray());
+            }
+        }
+
+        private static byte[] GetKey(int index)
+        {
+            return Encoding.UTF8.GetBytes("key:" + index);
+        }
+    }
+}
