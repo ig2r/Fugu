3f16523027be544cbbc82b996e050d70ea7ccb6e
Stephan Deininghaus <stephan@deininghaus.com>
2018-01-28 21:32:29 +0100

    Fix bug in SegmentLoadStrategy handling corrupted segments

--- src/Fugu.Core/Bootstrapping/SegmentLoadStrategy.cs
+++ src/Fugu.Core/Bootstrapping/SegmentLoadStrategy.cs
@@ -1,5 +1,4 @@
 ﻿using Fugu.Common;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -11,7 +10,7 @@ namespace Fugu.Bootstrapping
     /// </summary>
     public class SegmentLoadStrategy
     {
-        private readonly Segment[] _segments;
+        private readonly Queue<Segment> _segments;
 
         public SegmentLoadStrategy(IEnumerable<Segment> segments)
         {
@@ -20,32 +19,36 @@ namespace Fugu.Bootstrapping
             // Process segments in order, giving preference to segments that span larger generation
             // ranges since they are most likely more recent (e.g., created during compactions) than
             // segments that span smaller ranges
-            _segments = (from s in segments
-                         orderby
-                             s.MinGeneration ascending,
-                             s.MaxGeneration descending
-                         select s).ToArray();
+            _segments = new Queue<Segment>(from s in segments
+                                           orderby s.MinGeneration ascending,
+                                                   s.MaxGeneration descending
+                                           select s);
         }
 
         public bool GetNext(long maxGenerationLoaded, out Segment nextSegment, out bool requireValidFooter)
         {
-            // Find the first segment that starts at a generation greater than what we have loaded so far
-            nextSegment = _segments.FirstOrDefault(s => s.MinGeneration > maxGenerationLoaded);
-
-            if (nextSegment == null)
+            // Discard segments that have no unseen data to contribute
+            while (_segments.Count > 0 && _segments.Peek().MaxGeneration <= maxGenerationLoaded)
             {
+                _segments.Dequeue();
+            }
+
+            // Stop enumeration if no more segments are left
+            if (_segments.Count == 0)
+            {
+                nextSegment = null;
                 requireValidFooter = false;
                 return false;
             }
+
+            nextSegment = _segments.Dequeue();
 
             // If the segment following our chosen segment covers the same min genration range, we know that the
             // chosen segment was created through a compaction from the following segments, so we will be picky and
             // only accept it if it can be loaded entirely, with footer intact; otherwise, we'll skip it and restore
             // data from the smaller segments that follow it
-            var index = Array.IndexOf(_segments, nextSegment);
-            requireValidFooter =
-                index + 1 < _segments.Length &&
-                nextSegment.MinGeneration == _segments[index + 1].MinGeneration;
+            requireValidFooter = _segments.Count > 0 &&
+                _segments.Peek().MinGeneration == nextSegment.MinGeneration;
 
             return true;
         }
